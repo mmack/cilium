4f1a2e07be62a912a6a4fa0a817e2fad5f616eec
Martynas Pumputis <m@lambda.lt>
2020-08-05 11:55:57 +0200

    k8s: Add and parse LoadBalancerSourceRanges field
    It's going to be used by the loadBalancerSourceRanges check.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/k8s/slim/k8s/apis/core/v1/types.go
+++ pkg/k8s/slim/k8s/apis/core/v1/types.go
@@ -450,6 +450,14 @@ type ServiceSpec struct {
 	// +optional
 	ExternalIPs []string `json:"externalIPs,omitempty" protobuf:"bytes,5,rep,name=externalIPs"`
 
+	// If specified and supported by the platform, this will restrict traffic through the cloud-provider
+	// load-balancer will be restricted to the specified client IPs. This field will be ignored if the
+	// cloud-provider does not support the feature."
+	// More info:
+	// https://kubernetes.io/docs/tasks/access-application-cluster/configure-cloud-provider-firewall/
+	// +optional
+	LoadBalancerSourceRanges []string `json:"loadBalancerSourceRanges,omitempty" protobuf:"bytes,9,opt,name=loadBalancerSourceRanges"`
+
 	// Supports "ClientIP" and "None". Used to maintain session affinity.
 	// Enable client IP based session affinity.
 	// Must be ClientIP or None.
